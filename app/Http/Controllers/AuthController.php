c444d5785fb049ae01009baa791d676501936818
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-17 13:34:54 -0500

    sanctum install

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    
+    public function register(Request $request){
+        return User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "password" => Hash::make($request->password)
+        ]);
+    }
+    public function login(Request $request){
+        $credentials = $request->only('email', 'password'); 
+        if (Auth::attempt($credentials)) {
+            $user=Auth::user();
+            $token = $user->createToken('twilio-token')->plainTextToken;
+            return compact('user','token');
+        }
+    }
+}
