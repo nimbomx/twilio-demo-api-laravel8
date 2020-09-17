38c5aea683fa142625d9e44e535987296084e5a9
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-17 16:24:56 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::get('/twilio/callme', [TwilioController::class, 'callme']);
+Route::middleware('auth:sanctum')->get('/twilio/callme', [TwilioController::class, 'callme']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::->get('/user', function (Request $request) {
     return $request->user();
 });
