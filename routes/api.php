00a4d5d02b8ed3402833e62eba88837faaafeac8
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-17 16:26:08 -0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/twilio/callme', [TwilioController::class, 'callme']);
 
-Route::->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
