56572820510e673540f62c06e2b5157ff6c2188d
Ernesto Hernández <ernesto@nimbo.mx>
2020-09-21 11:38:24 -0500

    update twilio use of config

--- app/Http/Controllers/TwilioController.php
+++ app/Http/Controllers/TwilioController.php
@@ -10,29 +10,31 @@ use Illuminate\Http\Request;
 
 class TwilioController extends Controller
 {
-    public function callme(Request $request) {
+    public function callme(Request $request)
+    {
 
         $provider = Providers::findOrFail($request->id);
+        $config = $request->user()->config;
+        $to_number =  str_replace('-', '', filter_var($provider->phone, FILTER_SANITIZE_NUMBER_INT));
+        $ACCOUNT_SID    = $config->twilio_account_sid ?: env('TWILIO_ACCOUNT_SID');
+        $AUTH_TOKEN  =  $config->twilio_auth_token ?:  env('TWILIO_AUTH_TOKEN');
+        $TWILIO_NUMBER =  $config->twilio_number ?:  env('TWILIO_NUMBER');
 
-        $to_number =  str_replace('-','',filter_var($provider->phone, FILTER_SANITIZE_NUMBER_INT));
-        $ACCOUNT_SID    = env('TWILIO_ACCOUNT_SID');
-        $AUTH_TOKEN  = env('TWILIO_AUTH_TOKEN');
-        $TWILIO_NUMBER = env('TWILIO_NUMBER');
-        
-        
+
         $client = new Client($ACCOUNT_SID, $AUTH_TOKEN);
 
-        $client->account->calls->create(  
-        $to_number,
-        $TWILIO_NUMBER,
-        array(
-            "url" => "http://demo.twilio.com/docs/voice.xml"
+        $client->account->calls->create(
+            $to_number,
+            $TWILIO_NUMBER,
+            array(
+                "url" => "http://demo.twilio.com/docs/voice.xml"
             )
         );
-    
+
         return 'Calling...';
     }
-    public function connectCall(Request $request) {
+    public function connectCall(Request $request)
+    {
 
         $myfile = fopen("$request->id.txt", "w");
         fwrite($myfile, "a:waiting...");
@@ -40,53 +42,57 @@ class TwilioController extends Controller
 
         $provider = Providers::findOrFail($request->id);
 
-        $to_number =  str_replace('-','',filter_var($request->user()->phone, FILTER_SANITIZE_NUMBER_INT)); //GET THE MANAGER PHONE
-        $ACCOUNT_SID    = env('TWILIO_ACCOUNT_SID');
-        $AUTH_TOKEN  = env('TWILIO_AUTH_TOKEN');
-        $TWILIO_NUMBER = env('TWILIO_NUMBER');
-        
- 
+        $to_number =  str_replace('-', '', filter_var($request->user()->phone, FILTER_SANITIZE_NUMBER_INT)); //GET THE MANAGER PHONE
+
+        $config = $request->user()->config;
+
+        $ACCOUNT_SID    = $config->twilio_account_sid ?: env('TWILIO_ACCOUNT_SID');
+        $AUTH_TOKEN  =  $config->twilio_auth_token ?:  env('TWILIO_AUTH_TOKEN');
+        $TWILIO_NUMBER =  $config->twilio_number ?:  env('TWILIO_NUMBER');
+
+
         $client = new Client($ACCOUNT_SID, $AUTH_TOKEN);
 
         $host = $_SERVER['HTTP_HOST'];
-        
-        $encodedSalesPhone = urlencode(str_replace('-','',filter_var($provider->phone, FILTER_SANITIZE_NUMBER_INT))); 
+
+        $encodedSalesPhone = urlencode(str_replace('-', '', filter_var($provider->phone, FILTER_SANITIZE_NUMBER_INT)));
         //$outboundUri = "https://$host/api/twilio/outbound/$encodedSalesPhone/$request->id";
         $outboundUri = "https://$host/outbound.php?phone=$encodedSalesPhone&ID=$request->id";
 
         try {
             $call = $client->calls->create(
-                 $to_number,                 // The visitor's phone number
-                 $TWILIO_NUMBER,    // A Twilio number in your account
-                 [
-                     "method" => "GET",
-                     "url" => $outboundUri,   // public URL TwiML that handles the call
-                     "statusCallback" => "https://$host/api/twilio/admin_events/$request->id",
-                     "statusCallbackEvent" => ["initiated","ringing","answered","completed"],//"initiated ringing answered completed",
-                     "statusCallbackMethod" => "POST",
-                     
-                     ]
-                    );
-                    
-             print($call->sid);
-         } catch (Exception $e) {
-             // Failed calls will throw
-             return 'ERROR:' . $e;
-         }
+                $to_number,                 // The visitor's phone number
+                $TWILIO_NUMBER,    // A Twilio number in your account
+                [
+                    "method" => "GET",
+                    "url" => $outboundUri,   // public URL TwiML that handles the call
+                    "statusCallback" => "https://$host/api/twilio/admin_events/$request->id",
+                    "statusCallbackEvent" => ["initiated", "ringing", "answered", "completed"], //"initiated ringing answered completed",
+                    "statusCallbackMethod" => "POST",
+
+                ]
+            );
+
+            print($call->sid);
+        } catch (Exception $e) {
+            // Failed calls will throw
+            return 'ERROR:' . $e;
+        }
     }
 
-    public function outbound(Request $request,$phone,$id) {
+    public function outbound(Request $request, $phone, $id)
+    {
         $host = $_SERVER['HTTP_HOST'];
         $salesPhone = '+524446039460';
-        function generate_twiml_response() {
+        function generate_twiml_response()
+        {
             $response = new VoiceResponse();
             $response->say('Please, hold in the line, until the provider answers');
-            
+
             $dial = $response->dial('');
             //'statusCallbackEvent' => 'initiated ringing answered completed',
-            $dial->number($salesPhone
-            );
-/*
+            $dial->number($salesPhone);
+            /*
             [
                 "statusCallback" => "https://$host/api/twilio/provider_events/$id",
                 "statusCallbackEvent" => "initiated ringing answered completed",
@@ -95,43 +101,39 @@ class TwilioController extends Controller
 */
             return (string)$response;
         }
-        
+
         print_r(generate_twiml_response());
- 
-      
     }
-    public function adminEvents(Request $request,$id) {
+    public function adminEvents(Request $request, $id)
+    {
 
         $myfile = fopen("$id.txt", "w");
         fwrite($myfile, 'a:' . $request->CallStatus);
-      
     }
-    public function providerEvents(Request $request,$id) {
+    public function providerEvents(Request $request, $id)
+    {
 
         $myfile = fopen("$id.txt", "w");
         fwrite($myfile, 'p:' . $request->CallStatus);
-      
     }
 
 
-    public function sse(Request $request,$id) {
+    public function sse(Request $request, $id)
+    {
         header("Access-Control-Allow-Origin: *");
         header('Access-Control-Allow-Methods: GET');
         header("Access-Control-Allow-Headers: X-Requested-With");
         header('Content-Type: text/event-stream');
         header('Cache-Control: no-cache');
 
-        $file="$id.txt";
-        
+        $file = "$id.txt";
+
         $f = fopen($file, 'r');
         $status = fgets($f);
         fclose($f);
-        
+
         echo "data: {$status}\n\n";
         ob_flush();
         flush();
     }
-
-    
-    
 }
